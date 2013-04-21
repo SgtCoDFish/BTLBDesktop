f8055b2751c1a342a475722b611cc84aef1c1dde
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2013-04-21 16:23:53 +0100

    initial local commit

---
+++ src/com/sgtcodfish/BTLBDesktop/BTLBDesktop.java
@@ -0,0 +1,16 @@
+package com.sgtcodfish.BTLBDesktop;
+
+/**
+ * Desktop portion of turning an android device into a bluetooth headset.
+ * @author Ashley Davis (SgtCoDFish)
+ */
+public class BTLBDesktop {
+
+	/**
+	 * @param args Not used
+	 */
+	public static void main(String[] args) {
+		System.out.println("hihi");
+	}
+
+}
