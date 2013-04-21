37157bb5c68225cda3a9e2af6c322b1fc8576b95
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2013-04-21 16:32:18 +0100

    Added a debug function, updated README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ BTLBDesktop
 ===========
 
 BTLB - BlueTooth Loop Back - Turns an android device into a Bluetooth headset
+
+This is the desktop portion of BTLB, which one runs on a computer/laptop whose
+audio one wants to transmit.
+
+The android portion is at https://github.com/SgtCoDFish/BTLBAndroid
--- src/com/sgtcodfish/BTLBDesktop/BTLBDesktop.java
+++ src/com/sgtcodfish/BTLBDesktop/BTLBDesktop.java
@@ -1,5 +1,7 @@
 package com.sgtcodfish.BTLBDesktop;
 
+import javax.sound.sampled.*;
+
 /**
  * Desktop portion of turning an android device into a bluetooth headset.
  * @author Ashley Davis (SgtCoDFish)
@@ -10,7 +12,29 @@ public class BTLBDesktop {
 	 * @param args Not used
 	 */
 	public static void main(String[] args) {
-		System.out.println("hihi");
+		printMixers();
 	}
-
+	
+	/**
+	 * Prints a formatted list of all detected mixers and info about each.
+	 */
+	public static void printMixers() {
+		Mixer.Info infos[] = AudioSystem.getMixerInfo();
+		int count = 0;
+		
+		for(Mixer.Info i : infos) {
+			count++;
+			String title = "Device #" + count + "\n";
+			
+			int titleLength = title.length() - 1;
+			for(int dasher = 0; dasher < titleLength; dasher++) {
+				title += "-";
+			}
+			
+			System.out.println(title);
+			System.out.println(i.getName());
+			System.out.println(i.getDescription());
+			System.out.println();
+		}
+	}
 }
